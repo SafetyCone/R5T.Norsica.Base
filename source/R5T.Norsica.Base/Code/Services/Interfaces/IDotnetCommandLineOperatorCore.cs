65bcd5d962fcc6860fdc5158d3cf86f07dc89da5
MinexAutomation <minex.automation@gmail.com>
2019-10-29 22:32:20 -0700

    Added dotnet command line operator core and ease-of-use wrapper service interface types.

---
+++ source/R5T.Norsica.Base/Code/Services/Interfaces/IDotnetCommandLineOperatorCore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Norsica
+{
+    /// <summary>
+    /// The core dotnet command-line operator that will be implemented.
+    /// </summary>
+    public interface IDotnetCommandLineOperatorCore
+    {
+        void Publish(string projectFilePath, string outputDirectoryPath, string buildConfigurationName, string frameworkName);
+    }
+}
