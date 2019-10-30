b511b3eb303d17d740a5d4b43bc8a2d0ea1c6ac7
MinexAutomation <minex.automation@gmail.com>
2019-10-29 20:41:42 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Norsica.Base.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Norsica.Base.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
