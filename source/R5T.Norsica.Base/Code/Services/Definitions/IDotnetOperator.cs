d5f894aa6e7a71e5533a139cf4d153a4fff9ce77
MinexAutomation <minex.automation@gmail.com>
2019-12-14 21:37:18 -0800

    Added IDotnetOperator service definition interface type.

---
+++ source/R5T.Norsica.Base/Code/Services/Definitions/IDotnetOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Norsica
+{
+    /// <summary>
+    /// An operator that performs the same services as the "dotnet" executable.
+    /// </summary>
+    public interface IDotnetOperator
+    {
+    }
+}
