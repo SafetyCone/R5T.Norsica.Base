9c3b50bd5764f811b83094c87070a7e5b4e92f28
MinexAutomation <minex.automation@gmail.com>
2019-12-14 22:22:47 -0800

    Commented out IDotnetCommandLineOperator and IDotnetCommandLineOperatorCore service definition interface types.

--- source/R5T.Norsica.Base/Code/Services/Interfaces/IDotnetCommandLineOperator.cs
+++ source/R5T.Norsica.Base/Code/Services/Interfaces/IDotnetCommandLineOperator.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿//using System;
 
 
-namespace R5T.Norsica
-{
-    /// <summary>
-    /// Provides the services of the 'dotnet' command line tool.
-    /// A single wrapper class will implement this interface, and forward all extraneous calls (with the right adjustments) to a <see cref="IDotnetCommandLineOperatorCore"/> instance.
-    /// </summary>
-    public interface IDotnetCommandLineOperator
-    {
-        void Publish(string projectFilePath, string outputDirectoryPath, string buildConfigurationName, string frameworkName);
-        void PublishApp(string projectFilePath, string outputDirectoryPath, string buildConfigurationName);
-        void PublishApp(string projectFilePath, string outputDirectoryPath);
-    }
-}
+//namespace R5T.Norsica
+//{
+//    /// <summary>
+//    /// Provides the services of the 'dotnet' command line tool.
+//    /// A single wrapper class will implement this interface, and forward all extraneous calls (with the right adjustments) to a <see cref="IDotnetCommandLineOperatorCore"/> instance.
+//    /// </summary>
+//    public interface IDotnetCommandLineOperator
+//    {
+//        void Publish(string projectFilePath, string outputDirectoryPath, string buildConfigurationName, string frameworkName);
+//        void PublishApp(string projectFilePath, string outputDirectoryPath, string buildConfigurationName);
+//        void PublishApp(string projectFilePath, string outputDirectoryPath);
+//    }
+//}
--- source/R5T.Norsica.Base/Code/Services/Interfaces/IDotnetCommandLineOperatorCore.cs
+++ source/R5T.Norsica.Base/Code/Services/Interfaces/IDotnetCommandLineOperatorCore.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿//using System;
 
 
-namespace R5T.Norsica
-{
-    /// <summary>
-    /// The core dotnet command-line operator that will be implemented.
-    /// </summary>
-    public interface IDotnetCommandLineOperatorCore
-    {
-        void Publish(string projectFilePath, string outputDirectoryPath, string buildConfigurationName, string frameworkName);
-    }
-}
+//namespace R5T.Norsica
+//{
+//    /// <summary>
+//    /// The core dotnet command-line operator that will be implemented.
+//    /// </summary>
+//    public interface IDotnetCommandLineOperatorCore
+//    {
+//        void Publish(string projectFilePath, string outputDirectoryPath, string buildConfigurationName, string frameworkName);
+//    }
+//}
